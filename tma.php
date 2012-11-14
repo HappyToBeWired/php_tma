8eb2d597df985a747c2496ec5680d8720fffba0b
Alex <happytobewired@gmail.com>
2012-11-14 15:18:01 +0000

    just about done

--- tma.php
+++ tma.php
@@ -1,57 +1,51 @@
 <?php
-//delibaratlly suppressed theis undifined index warning
+//delibaratlly suppressed theis undifined offset notice 
 //http://www.dmxzone.com/go/13811/php-getting-notice-undefined-index/
-//error_reporting (E_ALL ^ E_NOTICE);
+error_reporting (E_ALL ^ E_NOTICE);
 require_once 'functions.php';
-//require_once 'classes.php';
+
 
 
 $log_file_dir = 'logs/';
 $process_files = new FindsExtractsAndFormats;
 $process_files->check_existance();
-var_dump($process_files->check_existance());
 // checks for existance of folder     
 
 
 if ($process_files->check_existance() === 'folder_exists'){
-    //echo 'hello Folder';
     $process_files->read_files();
      
     }
-    
-
 else{
     echo"The folder \"$log_file_dir\" is not here.";
 }
-/*reead individual files from log directory
- * 
- */
-$totalized=array('status code'=>'','article request'=>'','bandwidth consumed'=>'','total views'=>'');
+//reead individual files from log directory
 
-//var_dump($process_files->read_files($log_file_dir));
+$totalized=array('status code'=>'','article request'=>'','bandwidth consumed'=>'','total views'=>'');
+$unique_404 = array();
+
 $month_log= $process_files->read_files();
-var_dump($month_log);
 $i =0;
 while($i < count($month_log)){
-    //var_dump($month_log[$i]);
     $filtered_log_file=$process_files->extract_data_from_files($month_log[$i]);
-    //var_dump($filtered_log_file);
     $i++;
     $rupert= new EnumerateAndOutput;
     $rupert->enumerate($filtered_log_file);
-    //var_dump($rupert);
-    $rupert->return_array;
-    $totalized['status code'] = $totalized['status code'] + $rupert->return_array['status code'];
-    $totalized['article request'] = $totalized['article request'] + $rupert->return_array['article request'];
-    $totalized['bandwidth consumed'] = $totalized['bandwidth consumed'] + $rupert->return_array['bandwidth consumed'];
-    $totalized['total views'] = $totalized['total views'] + $rupert->return_array['total views'];
-    echo'<table>';
-    echo '<tr><td><h3>404 Status Codes, </h3></td><td><h3>Request of artical, </h3></td><td><h3>Bandwidth used, </h3></td><td><h3>Total views</h3></td></tr>';
-    echo '<tr><td>'.$rupert->return_array['status code'].'</td><td>'.$rupert->return_array['article request'].'</td><td>'.$rupert->return_array['bandwidth consumed'].'</td><td>'.$rupert->return_array['total views'].'</td></tr>';
-    echo'</table>'; 
-    var_dump($totalized);
+    $for_presentation = $rupert->return_array;
+    $totalized['status code'] = $totalized['status code'] + $for_presentation['status code'];
+    $totalized['article request'] = $totalized['article request'] + $for_presentation['article request'];
+    $totalized['bandwidth consumed'] = $totalized['bandwidth consumed'] + $for_presentation['bandwidth consumed'];
+    $totalized['total views'] = $totalized['total views'] + $for_presentation['total views'];
+    $unique_404 = array_merge($unique_404,$for_presentation['unique_coincidental_404']);
+    print_output($for_presentation['month'], $for_presentation['status code'], 
+            $for_presentation['article request'], $for_presentation['bandwidth consumed'], $for_presentation['total views'], $for_presentation['unique_coincidental_404']);
 }
-
+ $total_unique_heading = 'Totals';
+ 
+ print_output($total_unique_heading, $totalized['status code'], 
+            $totalized['article request'], $totalized['bandwidth consumed'], $totalized['total views'], array_unique($unique_404));     
+ 
+ $process_files->close_folder();
 
 
 
